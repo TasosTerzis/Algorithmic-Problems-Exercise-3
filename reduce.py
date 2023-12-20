ebf6776c9e8e26bbdb74d7c49bc2719f17de89ef
Tasos Terzis <tasosterzistt@gmail.com>
2023-12-20 16:40:02 +0200

    vamos

---
+++ reduce.py
@@ -0,0 +1,9 @@
+# code to reduce dimension of images
+
+import keras as kr
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+# Load the data set
+
--- test.py
+++
@@ -1,7 +0,0 @@
-import keras as kr
-import numpy as np
-import pandas as pd
-import tensorflow as tf
-
-# Load the data set
-
